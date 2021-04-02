ba801bdd233e8889097e4b5bf9ec8525e6de0322
duarte-santos <duarte.dos.santos@tecnico.ulisboa.pt>
2021-04-02 21:19:40 +0100

    Environment Generator by Dudu

--- src/main/java/pt/tecnico/sec/client/EnvironmentGenerator.java
+++ src/main/java/pt/tecnico/sec/client/EnvironmentGenerator.java
@@ -1,23 +1,35 @@
 package pt.tecnico.sec.client;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Random;
 
 public class EnvironmentGenerator {
 
+    public static final String ENVIRONMENT_PATH = "src/main/resources/environment.json";
+
     public static void main(String[] args) throws IOException {
 
-        // FIXME : receive and parse/validate args
-        String filename = "environment.json"; // FIXME : filepath
-        int nX = 10;
-        int nY = 10;
-        int epochCount = 10;
-        int userCount = 3;
+        try {
+            int nX = Integer.parseInt(args[0]);
+            int nY = Integer.parseInt(args[1]);
+            int epochCount = Integer.parseInt(args[2]);
+            int userCount  = Integer.parseInt(args[3]);
+            if (nX <= 0 || nY <= 0 || epochCount <= 0 || userCount <= 0)
+                throw new NumberFormatException();
 
+            writeJSON(nX, nY, epochCount, userCount);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("All arguments must be positive integers.");
+            System.out.println("USAGE: ./mvnw spring-boot:run -Dspring-boot.run.arguments=\"[nX] [nY] [epochCount] [userCount]\" -Dstart-class=pt.tecnico.sec.client.EnvironmentGenerator");
+        }
+
+    }
+
+    public static void writeJSON(int nX, int nY, int epochCount, int userCount) throws IOException {
         JSONObject environmentJSON = new JSONObject();
         // generate one grid for each epoch
         for (int epoch = 0; epoch < epochCount; epoch++) {
@@ -26,8 +38,8 @@ public class EnvironmentGenerator {
             for (int userId = 0; userId < userCount; userId++) {
                 // generate random location inside the grid limits
                 Random random = new Random();
-                int x = random.nextInt(nX + 1);
-                int y = random.nextInt(nY + 1);
+                int x = random.nextInt(nX); // FIXME : nX + 1 ?
+                int y = random.nextInt(nY); // FIXME : nY + 1 ?
                 // add location to user
                 JSONObject userJSON = new JSONObject();
                 userJSON.put("x", x);
@@ -39,7 +51,14 @@ public class EnvironmentGenerator {
             environmentJSON.put(String.valueOf(epoch), gridJSON);
         }
         // write environment to a JSON file
-        Files.write(Paths.get(filename), environmentJSON.toString().getBytes());
+        try (FileWriter file = new FileWriter(ENVIRONMENT_PATH)) {
+            file.write(environmentJSON.toJSONString());
+            file.flush();
+        }
+    }
+
+    public static void parseJSON() {
+        // TODO
     }
 
 }
